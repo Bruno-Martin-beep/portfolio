dd75dae80a263d13e61a04b582a09bb0d40a1a92
Bruno-Martin-beep <brunico040@gmail.com>
2022-05-19 11:55:20 -0300

    Update Contact

--- src/components/layout/Contact.js
+++ src/components/layout/Contact.js
@@ -10,10 +10,40 @@ const Contact = () => {
             const <span className="bold">sayHi</span> = {`{`}
           </p>
           <p className="cont_text cont_textSpace">
-            Mail: <a target="_blank" rel="noreferrer" href="mailto:brunomartin.dev@gmail.com">brunomartin.dev@gmail.com</a>,
+            Mail:{" "}
+            <a
+              className="cont_link"
+              target="_blank"
+              rel="noreferrer"
+              href="mailto:brunomartin.dev@gmail.com"
+            >
+              brunomartin.dev@gmail.com
+            </a>
+            ,
           </p>
-          <p className="cont_text cont_textSpace">Phone: <a target="_blank" rel="noreferrer" href="tel:+54 9 351 557-8940">+54 9 351 557-8940</a>,</p>
-          <p className="cont_text cont_textSpace">Github: <a target="_blank" rel="noreferrer" href="https://github.com/Bruno-Martin-beep">Bruno-Martin-beep</a></p>
+          <p className="cont_text cont_textSpace">
+            Phone:{" "}
+            <a
+              className="cont_link"
+              target="_blank"
+              rel="noreferrer"
+              href="tel:+54 9 351 557-8940"
+            >
+              +54 9 351 557-8940
+            </a>
+            ,
+          </p>
+          <p className="cont_text cont_textSpace">
+            Github:{" "}
+            <a
+              className="cont_link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Bruno-Martin-beep"
+            >
+              Bruno-Martin-beep
+            </a>
+          </p>
           <p className="cont_text">{`};`}</p>
         </div>
       </div>
