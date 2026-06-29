7d0aba7229e0aa129d2fc206cd39eb58a751ace0
bruno nicolas martin <brunomartin.dev@gmail.com>
2026-06-29 03:05:58 -0300

    [ ADD ] Solar 3D

--- src/mocks/projectsList.ts
+++ src/mocks/projectsList.ts
@@ -1,6 +1,6 @@
 import superShoe from "../assets/super shoe.webp";
-import shinji from "../assets/shinji.webp";
 import solar from "../assets/solar.webp";
+import solar3D from "../assets/solar_3d.webp";
 
 type Project = {
   title: string;
@@ -11,7 +11,17 @@ type Project = {
   className: string;
 };
 
+// Design solar farms by placing panels, sculpting terrain, and navigating real-world obstacles.
+
 const projectsList: Array<Project> = [
+  {
+    title: "Solar 3D",
+    description:
+      "Interactive solar-panel planning application featuring site layout and obstacle placement.",
+    image: solar3D,
+    linkLive: "https://bruno-martin-solar3d.vercel.app/",
+    className: "proj_bg_blue",
+  },
   {
     title: "Solar",
     description:
@@ -29,14 +39,14 @@ const projectsList: Array<Project> = [
     linkRepo: "https://github.com/Bruno-Martin-beep/zapaz",
     className: "",
   },
-  {
-    title: "Memory game",
-    description: "A mini-game of making pairs, where you can raise your score.",
-    image: shinji,
-    linkLive: "https://bruno-martin-beep.github.io/memory-game/",
-    linkRepo: "https://github.com/Bruno-Martin-beep/memory-game",
-    className: "proj_bg_blue",
-  },
+  // {
+  //   title: "Memory game",
+  //   description: "A mini-game of making pairs, where you can raise your score.",
+  //   image: shinji,
+  //   linkLive: "https://bruno-martin-beep.github.io/memory-game/",
+  //   linkRepo: "https://github.com/Bruno-Martin-beep/memory-game",
+  //   className: "proj_bg_blue",
+  // },
 ];
 
 export default projectsList;
